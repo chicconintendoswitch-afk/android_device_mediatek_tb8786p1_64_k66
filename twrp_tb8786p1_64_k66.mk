97fbd021f0acd40dd91dce0b45762cb8aa7729cd
chicconintendoswitch-afk <chicconintendoswitch@gmail.com>
2026-05-10 14:20:07 +0200

    Refactor TWRP device configuration for tb8786p1

--- twrp_tb8786p1_64_k66.mk
+++ twrp_tb8786p1_64_k66.mk
@@ -1,15 +1,11 @@
-# Inherit from common AOSP config
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Device identifier
 PRODUCT_NAME := twrp_tb8786p1_64_k66
 PRODUCT_DEVICE := tb8786p1_64_k66
 PRODUCT_MANUFACTURER := mediatek
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := tb8786p1_64_k66
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-# TWRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.touchscreen=himax \
     ro.sf.lcd_density=240
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
