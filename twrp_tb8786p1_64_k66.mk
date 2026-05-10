a6f08fac4b582696763d441dde2c8fbe27ecb20f
chicconintendoswitch-afk <chicconintendoswitch@gmail.com>
2026-05-10 13:48:14 +0200

    Modify device identifiers and manufacturer details
    Updated device properties for tb8786p1_64_k66.

--- twrp_tb8786p1_64_k66.mk
+++ twrp_tb8786p1_64_k66.mk
@@ -2,14 +2,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Device identifier
-PRODUCT_DEVICE := tb8786p1_64_k66
 PRODUCT_NAME := twrp_tb8786p1_64_k66
-PRODUCT_BRAND := PIXGOOD
-PRODUCT_MODEL := M103-EEA
-PRODUCT_MANUFACTURER := Shenzhen_Beishida
+PRODUCT_DEVICE := tb8786p1_64_k66
+PRODUCT_MANUFACTURER := mediatek
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := tb8786p1_64_k66
 
-PRODUCT_GMS_CLIENTID_BASE := android-mediatek
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # TWRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.touchscreen=himax \
